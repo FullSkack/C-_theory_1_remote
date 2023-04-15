04104b6b86fb80c913e94707edb9708f7c07939c
Martin_Power <f.drindrozhik@yandex.ru>
2023-04-15 22:13:46 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Сумма двух чисел, Вариант со вспомогательной переменной
-int numberA = 31;
-int numberВ = 5;
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberВ = new Random().Next(1,10);
+Console.WriteLine(numberВ);
 int result = numberA + numberВ;
 Console.WriteLine(result);

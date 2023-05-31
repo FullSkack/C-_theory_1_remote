db78670f2ed6f281e7e6f2278ec78c206d2fc753
Martin_Power <f.drindrozhik@yandex.ru>
2023-05-31 21:44:38 +0300

    commit

---
+++ Lection_2/Example006_MethodArray/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[10]; // задаем колличесвто эллементов массива
+
+void FillArray(int[] collection) // Объявляем метод и переменные для работы
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length) // цикл перебора и присвоения случайного значения элементу
+    {
+        collection[index] = new Random().Next(1, 10);
+        index ++;
+    }
+}
+
+void PrintArray(int[] massiv) // Объявляем метод и переменные для работы
+{
+    int count = massiv.Length;
+    int position = 0;
+
+    while (position < count) // цикл перебора и вывода в консоль соответсвующего элемента
+    {
+        Console.Write(massiv[position]);
+        position ++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);

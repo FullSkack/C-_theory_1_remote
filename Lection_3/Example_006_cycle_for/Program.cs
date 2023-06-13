11ee763d40ee87e9f2b63791e70193486265b9c3
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-13 22:31:43 +0300

    commit

---
+++ Lection_3/Example_006_cycle_for/Program.cs
@@ -0,0 +1,13 @@
+﻿// Использование цикла for
+string Metod4(int count, string text)
+{ 
+    string result = String.Empty;
+    // Цикл фор в котором задется переменная счетчик для перебора действий.
+    for (int i = 0; i<count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Metod4(10, "asdf"); // задаем переменную в которой вызываем метод
+Console.WriteLine(res);

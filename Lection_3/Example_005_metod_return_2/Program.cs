d503db1f72167c643cdc62ae1cf37cbcf94d3b62
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-13 22:19:42 +0300

    commit

---
+++ Lection_3/Example_005_metod_return_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// метод принимает на вход две переменные, 
+//на выхож дает значение с типом string
+string Metod4(int count, string text)
+{ 
+    int i = 0;
+    // переменная в которую записываем результ, изначльно пустая строка
+    string result = String.Empty;
+    while (i<count) 
+    // цикл для компановки текста count раз
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Metod4(10, "asdf"); // задаем переменную в которой вызываем метод
+Console.WriteLine(res);

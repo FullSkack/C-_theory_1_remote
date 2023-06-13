a37102a397437e75beda12024ab645b0758a0c17
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-13 21:39:30 +0300

    commit

---
+++ Lection_3/Example_002_return_metod/Program.cs
@@ -0,0 +1,6 @@
+﻿void Metod2(string msg) // объявление метода в скобках переменныи и их тип
+{
+    System.Console.WriteLine(msg);
+}
+
+Metod2 ("Message"); // вызов метода с задаваем значением переенной

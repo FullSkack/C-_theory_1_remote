11dad9b324e636029e41eb50c46c8bd6ef4005a6
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-16 01:16:33 +0300

    commit

--- Lection_1/Example002_НelloUser/Program.cs
+++ Lection_1/Example002_НelloUser/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Запрос данных у пользователя и вывод на экран
 Console.Write("Введите Ваше имя ");
-string username = Console.ReadLine();
+string username = Convert.ToString(Console.ReadLine());
 Console.Write("Привет, ");
 Console.Write(username);
 

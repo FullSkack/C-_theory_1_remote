26af4ec004dbd36da46b844e3c7cecedf02714c0
Martin_Power <f.drindrozhik@yandex.ru>
2023-06-28 15:43:00 +0300

    commit

---
+++ Lection_4/Example_005_Fact_Recur/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+double Factorial (int n)
+{
+    // Ветвление факториала
+    if(n == 1) return 1;
+    else return n * Factorial (n-1);
+}
+ // цикл для вывода значения кажого факториала
+for (int i = 1; i < 40; i++)
+{
+    System.Console.WriteLine(Factorial(i));
+}

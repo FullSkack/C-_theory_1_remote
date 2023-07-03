ed63144961c834d7c47600c975553258d7ed7bf7
Martin_Power <f.drindrozhik@yandex.ru>
2023-07-04 01:42:35 +0300

    commit

--- Lection_1/Example002_НelloUser/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// Запрос данных у пользователя и вывод на экран
-Console.Write("Введите Ваше имя ");
-string username = Convert.ToString(Console.ReadLine());
-Console.Write("Привет, ");
-Console.Write(username);
-
--- Lection_3/Example_002_metod_void_2/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿void Metod2(string msg) // объявление метода в скобках переменныи и их тип
-{
-    System.Console.WriteLine(msg);
-}
-
-Metod2 ("Message"); // вызов метода с задаваем значением переенной
